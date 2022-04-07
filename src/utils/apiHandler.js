c526aa27ea6710ac51ea96ac42ea9cf7ecafb1c3
mauro <ms.digital990@gmail.com>
2022-04-07 22:26:40 +0200

    WIP

--- src/utils/apiHandler.js
+++ src/utils/apiHandler.js
@@ -1,7 +1,6 @@
 import fetch from 'isomorphic-fetch'
 
-async function apiHandler(token, url, options) {
-    console.log({ options })
+async function apiHandler(token, url = '', options) {
     const data = await fetch(`${process.env.REACT_APP_API_URL}${url}`, {
         ...options,
         headers: {
